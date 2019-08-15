e51b3d11576c6aba6247cd10e56db790e5073dd4
Michael <3yebmb@gmail.com>
2019-08-15 21:03:58 +0300

    add sign up and adding user

--- app/src/main/java/dev/m13d/mygeo/MainActivity.java
+++ app/src/main/java/dev/m13d/mygeo/MainActivity.java
@@ -1,14 +1,119 @@
 package dev.m13d.mygeo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import dev.m13d.mygeo.Models.User;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnSignUp;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
+    RelativeLayout root;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn = findViewById(R.id.btnSignIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+        root = findViewById(R.id.root_element);
+
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSignUpWindow();
+            }
+        });
+    }
+
+    private void showSignUpWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Sign Up");
+        dialog.setMessage("Fill in all fields for sign up");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View signUp_window = inflater.inflate(R.layout.sign_up_window, null);
+        dialog.setView(signUp_window);
+
+        final MaterialEditText email = signUp_window.findViewById(R.id.emailField);
+        final MaterialEditText pass = signUp_window.findViewById(R.id.passField);
+        final MaterialEditText name = signUp_window.findViewById(R.id.nameField);
+        final MaterialEditText phone = signUp_window.findViewById(R.id.phoneField);
+
+        dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        dialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                if (TextUtils.isEmpty(email.getText().toString())) {
+                    Snackbar.make(root, "Input your e-mail", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(name.getText().toString())) {
+                    Snackbar.make(root, "Input your name", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(phone.getText().toString())) {
+                    Snackbar.make(root, "Input your phone", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (pass.getTag().toString().length() < 5) {
+                    Snackbar.make(root, "Input password bigger than 5 signs", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            User user = new User();
+                            user.setEmail(email.getText().toString());
+                            user.setName(name.getText().toString());
+                            user.setPass(pass.getText().toString());
+                            user.setPhone(phone.getText().toString());
+
+                            users.child(user.getEmail()).setValue(user)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Snackbar.make(root, "User was added", Snackbar.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    });
+
+            }
+        });
+        dialog.show();
     }
 }
